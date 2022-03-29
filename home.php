9368856e7e96ff323bdb3d8981d1cc8321520c0e
Asfandyarkhanafridi <masfandy9@gmail.com>
2022-03-29 07:54:02 -0700

    asfand commit

--- home.php
+++ home.php
@@ -7,8 +7,8 @@
             
             <!-- Hero Content -->
             <div class="home-content">
-                <h1 class="hs-line-4 mb-30 mb-xs-20 wow fadeInUpShort" data-wow-delay=".1s">Awards Winning Creative Studio</h1>
-                <h2 class="hs-line-7 mb-60 mb-xs-40 wow fadeInUpShort" data-wow-delay=".2s">We turn ideas into reality</h2>
+                <h2 class="hs-line-7 mb-30 mb-xs-40 wow fadeInUpShort" data-wow-delay=".1s">Impact Enterprises</h2>
+                <h1 class="hs-line-4 mb-90 mb-xs-20 wow fadeInUpShort" data-wow-delay=".2s">Tax Consultant, General Order Supplier, Website Development and Event Management</h1>
                 <div class="local-scroll mb-20 wow fadeInUpShort" data-wow-delay=".3s">
                     <a href="#about" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">Learn More</a>
                     <a href="pages-contact-1.html" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">Start Project</a>
@@ -26,8 +26,7 @@
             
             <!-- Hero Content -->
             <div class="home-content">
-                <h1 class="hs-line-4 mb-30 mb-xs-20">Get the new experience with</h1>
-                <h2 class="hs-line-7 mb-60 mb-xs-40">Creative Design Agency</h2>
+                <h2 class="hs-line-7 mb-60 mb-xs-40">Tax Consultancy</h2>
                 <div class="local-scroll mb-20">
                     <a href="#about" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">See More</a>
                     <a href="https://www.youtube.com/watch?v=jTea_8Fk5Ns" class="btn btn-mod btn-w btn-medium btn-round mx-md-1 lightbox mfp-iframe">Play Reel</a>
@@ -45,8 +44,7 @@
             
             <!-- Hero Content -->
             <div class="home-content">
-                <h1 class="hs-line-4 mb-30 mb-xs-20">Create Your Dream</h1>
-                <h2 class="hs-line-7 mb-60 mb-xs-40">With Amazing Design</h2>
+                <h2 class="hs-line-7 mb-60 mb-xs-40">General order Supplier</h2>
                 <div class="local-scroll mb-20">
                     <a href="pages-pricing-1.html" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">Get Pricing</a>
                 </div>
@@ -57,5 +55,38 @@
     </section>
     <!-- End Slide Item -->
 
+    <!-- Slide Item -->
+    <section class="home-section bg-scroll bg-dark-alfa-50 light-content" data-background="images/full-width-images/section-bg-13.jpg">
+        <div class="container min-height-100vh d-flex align-items-center pt-100 pb-100">
+            
+            <!-- Hero Content -->
+            <div class="home-content">
+                <h2 class="hs-line-7 mb-60 mb-xs-40">Website Development</h2>
+                <div class="local-scroll mb-20">
+                    <a href="pages-pricing-1.html" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">Get Pricing</a>
+                </div>
+            </div>
+            <!-- End Hero Content -->
+            
+        </div>
+    </section>
+    <!-- End Slide Item -->
+
+    <!-- Slide Item -->
+        <section class="home-section bg-scroll bg-dark-alfa-50 light-content" data-background="images/full-width-images/section-bg-13.jpg">
+        <div class="container min-height-100vh d-flex align-items-center pt-100 pb-100">
+            
+            <!-- Hero Content -->
+            <div class="home-content">
+                <h2 class="hs-line-7 mb-60 mb-xs-40">Event Management</h2>
+                <div class="local-scroll mb-20">
+                    <a href="pages-pricing-1.html" class="btn btn-mod btn-w btn-medium btn-round mx-md-1">Get Pricing</a>
+                </div>
+            </div>
+            <!-- End Hero Content -->
+            
+        </div>
+    </section>
+    <!-- End Slide Item -->
 </div>
 <!-- End Fullwidth Slider -->
